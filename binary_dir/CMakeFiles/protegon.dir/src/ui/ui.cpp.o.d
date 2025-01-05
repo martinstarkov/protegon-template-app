2ab5bdde2c655500b2c06695d7518cb6657707f3
Martin Starkov <emailregisterlogin@gmail.com>
2025-01-04 21:46:57 -0500

    Revert "init"
    This reverts commit 1b76ae0270895cc994c81b393aa2d6b2e24abae9.

--- binary_dir/CMakeFiles/protegon.dir/src/ui/ui.cpp.o.d
+++
@@ -1,3 +0,0 @@
-binary_dir/CMakeFiles/protegon.dir/src/ui/ui.cpp.o: \
-  /Users/pandora/Documents/GitHub/protegon/src/ui/ui.cpp \
-  /Users/pandora/Documents/GitHub/protegon/src/ui/ui.h
--- src/application.cpp
+++ src/application.cpp
@@ -1,195 +1,75 @@
 #include "protegon/protegon.h"
-#include "vfx/light.h"
-
 
 using namespace ptgn;
 
-constexpr const V2_int resolution{ 1280, 720 };
-
-static void TransitionScene(std::string_view from, std::string_view to, milliseconds duration = milliseconds{ 250 }) {
-	game.scene.TransitionActive(from, to, { TransitionType::Fade, milliseconds{ 1000 } });
-}
-
-
-class BlockMenu : public Scene {
+class Paint : public Scene {
 public:
-	static const size_t num_buttons = 16;
-	//Button b_mixer;
-	//Button b_heater;
-	//Button b_cooler;
-	//Button b_disolver;
-	//Button b_filter;
-	//Button b_evaporator;
-	//Button b_crystaliser;
-	//Button b_distiller;
-	//Button b_titrater;
-	//Button b_seperator;
-	//Button b_observer;
-	//Button b_b1;
-	//Button b_b2;
-	//Button b_b3;
-	//Button b_b4;
-	//Button b_b5;
-	//Text mixer{"Mixer", color::Gray};
-	//Text heater{"Heater", color::Gray};
-	//Text cooler{"Cooler", color::Gray};
-	//Text disolver{"Disolver", color::Gray};
-	//Text filter{"Filter", color::Gray};
-	//Text evaporator{"Evaporator", color::Gray};
-	//Text crystaliser{"Crystaliser", color::Gray};
-	//Text distiller{"Distiller", color::Gray};
-	//Text titrater{"Titrater", color::Gray};
-	//Text seperator{"Seperator", color::Gray};
-	//Text observer{"Observer", color::Gray};
+	Grid<int> outer_grid{ { 36, 36 } };
+	Grid<int> inner_grid{ { 36, 36 } };
+	Grid<int> grid{ { 36, 36 } };
+	V2_int tile_size{ 20, 20 };
 
-
-	//std::array<Text, num_buttons> button_names {mixer, heater, cooler, disolver, filter, evaporator, crystaliser, distiller, titrater, seperator, observer, b1, b2, b3, b4, b5};
-	//std::array<Button, num_buttons> button_group {b_mixer, b_heater, b_cooler, b_disolver, b_filter, b_evaporator, b_crystaliser, b_distiller, b_titrater, b_seperator, b_observer, b_b1, b_b2, b_b3, b_b4, b_b5};
-	//std::array<std::string_view, num_buttons> button_names {"Mixer", "Heater", "Cooler", "Disolver", "Filter", "Evaporator", "Crystaliser", "Distiller", "Titrater", "Sperator", "Observer", "Blank1", "Blank2", "Blank3", "Blank4", "Blank5"};
-	std::vector<Button> buttons;
-
-	BlockMenu() {}
-
-	Button CreateButton(const std::string_view& name) {
-		Button b;
-		b.Set<ButtonProperty::Toggleable>(true);
-		b.Set<ButtonProperty::BackgroundColor>(color::White);
-		b.Set<ButtonProperty::BackgroundColor>(color::DarkBlue, ButtonState::Hover);
-		b.Set<ButtonProperty::Text>(Text{name, color::Gray});
-		b.Set<ButtonProperty::OnActivate>([=]() {
-			PTGN_LOG("Pressed button: ", name);
-		});
-		return b;
-	}
-
-	//Button CreateButton(const V2_float& center, const Text name) {
-		//Button b;
-		//b.SetRect(Rect{ center, { 100, 50 }, Origin::Center });
-		//b.Set<ButtonProperty::Toggleable>(true);
-		//b.Set<ButtonProperty::BackgroundColor>(color::White);
-		//b.Set<ButtonProperty::BackgroundColor>(color::Red, ButtonState::Hover);
-		//b.Set<ButtonProperty::Text>(name);
-		//b.Set<ButtonProperty::OnActivate>([=]() {
-			//PTGN_LOG("Pressed button at: ", center);
-		//});
-		//return b;
-	//}
-
-    void Init() override {
-		buttons.emplace_back(CreateButton("Mixer"));
-		buttons.emplace_back(CreateButton("Heater"));
-		buttons.emplace_back(CreateButton("Cooler"));
-		buttons.emplace_back(CreateButton("Dissolver"));
-		buttons.emplace_back(CreateButton("Filter"));
-		buttons.emplace_back(CreateButton("Evaporator"));
-		buttons.emplace_back(CreateButton("Crystaliser"));
-		buttons.emplace_back(CreateButton("Distiller"));
-		buttons.emplace_back(CreateButton("Titrator"));
-		buttons.emplace_back(CreateButton("Seperator"));
-		buttons.emplace_back(CreateButton("Observer"));
-		buttons.emplace_back(CreateButton("Observer1"));
-		buttons.emplace_back(CreateButton("Observer2"));
-		buttons.emplace_back(CreateButton("Observer3"));
-		buttons.emplace_back(CreateButton("Observer4"));
-		buttons.emplace_back(CreateButton("Observer5"));
-		game.window.SetTitle("BlockMenu");
-		game.window.SetSize(resolution);
-		int x_pad = 250;
-		int y_pad = 150;
-		int idx = 0;
-		for (int i = 0; i < num_buttons / 4; ++i) {
-			for (int j = 0; j < num_buttons / 4; ++j) {
-				buttons[idx].SetRect(Rect{{125.0f + x_pad * i, 125.0f + y_pad * j}, {100, 50}, Origin::Center});	
-				idx++;
-			}
-		}
-	}
-
-	void Update() final {
-		for (Button b : buttons){
-			b.Draw();
-		}
-	}
-};
-
-
-class PlacementMenu : public Scene {
-public:
-	Button dragging;
-	ToggleButtonGroup buttons;
-	
-	PlacementMenu() {}
+	Texture grass{ "resources/example_texture/grass.png" };
 
 	void Init() override {
-		game.window.SetTitle("PlacementMenu");
-		game.window.SetSize(resolution);
-	}
-	void Update() final {
-		V2_int mouse_pos = game.input.GetMousePosition();
-        mouse_pos.Draw(Color{ 0, 0, 255, 30 }, 2.0f);
-
-
-		if (game.input.KeyDown(Key::B)) {
-				auto id = buttons.Size() + 1;
-				std::string id_s = std::to_string(id);
-		}
-
-		if (game.input.MouseDown(Mouse::Left)) {
-			buttons.ForEachValue([=](Button& b) {
-				auto rect = b.GetRect();
-				if (rect.Overlaps(mouse_pos)) {
-					dragging = b;
-				}
-			});
-		} else if (game.input.MouseUp(Mouse::Left)) {
-			dragging = {};
-		}
-
-		if (dragging != Button{}) {
-			auto rect = dragging.GetRect();
-			rect.position = mouse_pos;
-			dragging.SetRect(rect);
-		}
-		Line line1{ dragging.GetRect().position, mouse_pos };
-		line1.Draw(color::Purple, 3.0f);
-        //auto c = camera.GetPrimary();
-		buttons.Draw();
-		
-	}
-};
-
-
-class Simulink : public Scene {
-public:
-	Simulink() {
-		game.window.SetTitle("Simulink");
-		game.window.SetSize(resolution);
-		game.scene.Load<BlockMenu>("BlockMenu");
-		game.scene.Load<PlacementMenu>("PlacementMenu");
+		outer_grid.Fill(0);
 	}
 
-    void Init() override {
-		game.scene.AddActive("BlockMenu");
-	}
-
-    void Shutdown() override {
-		game.scene.RemoveActive("BlockMenu");
-		game.scene.RemoveActive("PlacementMenu");
-		//game.draw.SetTarget();
-	}
-
+	bool toggle = true;
 
 	void Update() override {
-		if (game.input.KeyDown(Key::I)) {
-			TransitionScene("BlockMenu", "PlacementMenu");
+		std::vector<int> cells_without;
+		cells_without.resize(static_cast<std::size_t>(outer_grid.GetLength()), -1);
+		outer_grid.ForEachIndex([&](int index) {
+			int value = outer_grid.Get(index);
+			if (value != 1) {
+				cells_without[static_cast<std::size_t>(index)] = value;
+			}
+		});
+		inner_grid = Grid<int>{ outer_grid.GetSize(), cells_without };
+		if (game.input.KeyDown(Key::B)) {
+			toggle = !toggle;
+		}
+		if (toggle) {
+			grid = outer_grid;
+		} else {
+			grid = inner_grid;
 		}
 
-	}
+		V2_int mouse_pos = game.input.GetMousePosition();
 
+		V2_int mouse_tile = mouse_pos / tile_size;
+		Rect mouse_box{ mouse_tile * tile_size, tile_size, Origin::TopLeft };
+
+		if (grid.Has(mouse_tile)) {
+			if (game.input.MousePressed(Mouse::Left)) {
+				outer_grid.Set(mouse_tile, 1);
+			}
+			if (game.input.MousePressed(Mouse::Right)) {
+				outer_grid.Set(mouse_tile, 0);
+			}
+		}
+
+		grid.ForEachCoordinate([&](const V2_int& p) {
+			Color c = color::Red;
+			Rect r{ V2_int{ p.x * tile_size.x, p.y * tile_size.y }, tile_size, Origin::TopLeft };
+			if (grid.Has(p)) {
+				switch (grid.Get(p)) {
+					case 0: r.Draw(color::Gray, -1.0f); break;
+					case 1: grass.Draw(r); break;
+				}
+			}
+		});
+		if (grid.Has(mouse_tile)) {
+			mouse_box.Draw(color::Yellow);
+		}
+		Text{ ToString(mouse_tile), color::Red }.Draw(Rect{ mouse_box.Center(), {}, Origin::Center }
+		);
+	}
 };
 
-int main(int c, char** v) {
-	game.Init();
-	game.scene.LoadActive<Simulink>("Simulink");
+int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
+	game.Init("paint: left click to draw; right click to erase; B to flip color", { 720, 720 });
+	game.scene.LoadActive<Paint>("paint");
 	return 0;
 }
